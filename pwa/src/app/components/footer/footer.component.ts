776ee6936aaefd2e1c9e5f8bcb0e419d966f706f
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-01-11 17:41:33 +0100

    footer component + scss + responsive

--- pwa/src/app/components/footer/footer.component.ts
+++ pwa/src/app/components/footer/footer.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhoneAlt} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
+
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
+  faEnvelope = faEnvelope;
+  faPhoneAlt = faPhoneAlt;
 
   constructor() { }
 
