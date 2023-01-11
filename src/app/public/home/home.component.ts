c7aed285af4d542eaa2589358caffcd77dbba4f9
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-01-11 05:34:50 +0100

    Home Component

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
 
   constructor() { }
 
