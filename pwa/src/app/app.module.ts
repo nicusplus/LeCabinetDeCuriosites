95db0de2014eb472534765769ea80d70a7e9bf2c
EmmanuelLefevre <waiasiaa@protonmail.com>
2023-01-14 02:55:56 +0100

    scroll-to-top component

--- pwa/src/app/app.module.ts
+++ pwa/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top/scroll-to-top.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    ScrollToTopComponent
   ],
   imports: [
     BrowserModule,
---
+++ pwa/src/app/components/scroll-to-top/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  templateUrl: './scroll-to-top.component.html',
+  styleUrls: ['./scroll-to-top.component.scss']
+})
+export class ScrollToTopComponent implements OnInit {
+
+  faAngleDoubleUp = faAngleDoubleUp;
+
+  showScroll: boolean | undefined;
+  showScrollHeight = 200;
+  hideScrollHeight = 200;
+
+  constructor() { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (
+      (
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop
+      ) > this.showScrollHeight
+    ) {
+      this.showScroll = true;
+    } else if (this.showScroll &&
+      (
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop) < this.hideScrollHeight
+    ) {
+      this.showScroll = false;
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  scrollToTop() {
+    (function smoothscroll() {
+      const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        // window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 5));
+      }
+    })();
+  }
+
+}
